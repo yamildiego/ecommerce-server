62ff8557ecb48a69e54b2d37bbe8a606241b504e
yamildiego <yamildiego@gmail.com>
2023-01-11 14:46:40 +1100

    fix: env varibles

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 const dotenv = require("dotenv").config();
 module.exports = {
-  IS_PRODUCTION: true,
   STRIPE_SK: process.env.STRIPE_SK,
   DOMAIN: process.env.DOMAIN,
   SHIPPING_PRICE: process.env.SHIPPING_PRICE,
-  LOCAL_MONGODB_CONNECTION: process.env.LOCAL_MONGODB_CONNECTION,
-  SERVER_MONGODB_CONNECTION: process.env.SERVER_MONGODB_CONNECTION,
+  MONGODB_CONNECTION: process.env.MONGODB_CONNECTION,
 };
--- index.js
+++ index.js
@@ -7,34 +7,30 @@ const routesApi = require("./src/routes/api");
 const routesProducts = require("./src/routes/products");
 const config = require("./config");
 
-let svr = config.IS_PRODUCTION ? "/server_0" : "";
+mongoose.connect(config.MONGODB_CONNECTION, { useNewUrlParser: true }).then(() => {
+  const app = express();
+  app.use(express.json());
+  app.use(session({ secret: "8JyLd{C7fk]JFDD>", saveUninitialized: true, resave: true }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose
-  .connect(config.IS_PRODUCTION ? config.SERVER_MONGODB_CONNECTION : config.LOCAL_MONGODB_CONNECTION, { useNewUrlParser: true })
-  .then(() => {
-    const app = express();
-    app.use(express.json());
-    app.use(session({ secret: "8JyLd{C7fk]JFDD>", saveUninitialized: true, resave: true }));
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-
-    app.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", config.DOMAIN);
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization"
-      );
-      res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE");
-      res.header("Access-Control-Allow-Credentials", true);
-      next();
-    });
-
-    app.use(`${svr}/`, routesApi);
-    app.use(`${svr}/products`, routesProducts);
-
-    const port = process.env.port || 5000;
-
-    app.listen(port, () => {
-      console.log("Server has started!");
-    });
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", config.DOMAIN);
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
   });
+
+  app.use(`/`, routesApi);
+  app.use(`/products`, routesProducts);
+
+  const port = process.env.port || 5000;
+
+  app.listen(port, () => {
+    console.log("Server has started!");
+  });
+});
